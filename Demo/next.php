931c8b93a761b8d336ffe33c7ee097e4aeac9fa7
Atharv Gurav <159026353+atharvgurav35@users.noreply.github.com>
2026-03-11 11:37:20 +0530

    Refactor cost sheet PHP script and HTML structure

--- Demo/next.php
+++ Demo/next.php
@@ -1,4 +1,11 @@
 <?php
+
+$conn = mysqli_connect("localhost","root","","costsheet");
+
+if(!$conn){
+die("Database Connection Failed");
+}
+
 if(isset($_POST['submit']))
 {
 
@@ -25,6 +32,22 @@ $total_dev = $agreement + $mseb + $society + $club;
 $gst = $agreement * 0.13;
 
 $total = $total_dev + $stamp + $maint + $reg + $gst;
+$sql = "INSERT INTO customers
+(customer_name,mobile_number,flat_no,
+area,rate,carpet_area,agreement_cost,
+mseb,society,clubhouse,total_paid,
+stamp_duty,maintenance,registration,
+gst,total_cost)
+
+VALUES
+
+('$customer','$mobile','$flat',
+'$area','$rate','$carpet','$agreement',
+'$mseb','$society','$club','$total_dev',
+'$stamp','$maint','$reg',
+'$gst','$total')";
+mysqli_query($conn,$sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -36,7 +59,7 @@ $total = $total_dev + $stamp + $maint + $reg + $gst;
 <style>
 
 body{
-font-family: Arial;
+font-family:Arial;
 text-align:center;
 }
 
@@ -71,6 +94,17 @@ font-size:16px;
 button{
 display:none;
 }
+
+body{
+-webkit-print-color-adjust:exact;
+print-color-adjust:exact;
+}
+}
+
+ul{
+text-align:left;
+width:70%;
+margin:auto;
 }
 
 </style>
@@ -78,17 +112,17 @@ display:none;
 </head>
 
 <body>
-<hr>
+
 <h2>GANGA FERNHILL PHASE</h2>
 <h3>UNDRI</h3>
-<hr>
-<p style="text-align:left; margin:auto; font-size:16px;" >
+
+<p>
 Customer Name: <?php echo $customer; ?><br>
 Mobile Number: <?php echo $mobile; ?><br>
 Flat: <?php echo $flat; ?>
 </p>
 
-<h3 style = "background:grey;padding :10px;">COSTSHEET DETAILS & CALCULATIONS</h3>
+<h3>COSTSHEET DETAILS & CALCULATIONS</h3>
 
 <table>
 
@@ -164,30 +198,26 @@ Flat: <?php echo $flat; ?>
 
 </table>
 
+<br>
+<hr>
+
+<h3 style="text-align:left;margin-left:120px;">Note :</h3>
+
+<ul>
+<li>Cheque Should Be drawn In favor of " Meenamani Ganga Builder LLP"</li>
+<li>1% TDS will be applicable For agreement Value more than 50Lac</li>
+<li>GST 12% on Agreement Cost, GST 18% on Maintenance Cost</li>
+<li>Rates are subject to change without prior notice.</li>
+<li>Govt. Taxes May vary as per Govt. Policies and are to be paid as per actual.</li>
+<li>Rates are calculated after giving 200/- per sqft GST Discount</li>
+<li>Legal Charges Of Rs.10000/- to be paid at the time of Agreement Registration</li>
+</ul>
+
+<button onclick="window.print()">Download / Print</button>
 
 </body>
 </html>
 
 <?php
-}?>
-<hr>
-
-<h3 style="text-align:left;">Note :</h3>
-
-<ul style="text-align:left; width:80%; margin:auto; font-size:16px;">
-<li>Cheque Should Be drawn In favor of " Meenamani Ganga Builder LLP"</li>
-
-<li>1% TDS will be applicable For agreement Value more than 50Lac</li>
-
-<li>GST 12% on Agreement Cost, GST 18% on Maintenance Cost</li>
-
-<li>Rates are subject to change without prior notice.</li>
-
-<li>Govt. Taxes May vary as per Govt. Policies and are to be paid as per actual.</li>
-
-<li>Rates are calculated after giving 200/- per sqft GST Discount</li>
-
-<li>Legal Charges Of Rs.10000/- to be paid at the time of Agreement Registration</li>
-</ul>
-<button onclick="window.print()">Download / Print</button>
-
+}
+?>
